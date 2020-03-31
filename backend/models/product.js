b50f98da14912b05e4b5273b61c353c9d1752e27
prashantpaddune <elonpras@my.smccd.edu>
2020-03-31 18:35:40 +0530

    create product schema

---
+++ backend/models/product.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const {ObjectId} = mongoose.Schema;
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 30
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    category: {
+        type: ObjectId,
+        ref: "Category",
+        required: true
+    },
+    stock: {
+        type: Number
+    },
+    sold: {
+        type: Number,
+        default: 0
+    },
+    photo: {
+        data: Buffer,
+        contentType: String
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Product", productSchema);
