b2c5a96cf58760e063a7ae161e9daf4691539c4d
prashantpaddune <elonpras@my.smccd.edu>
2020-04-01 20:58:03 +0530

    add middlewares

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 
 const express = require('express');
 const mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -17,6 +20,10 @@ mongoose
     .catch((error) => {
         console.error("DB GOT CRASH")
     });
+
+    app.use(bodyParser.json());
+    app.use(cookieParser());
+    app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
