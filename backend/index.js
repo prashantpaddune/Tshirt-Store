ea6c4221f7c793c375059049d455619f59e1e2b7
prashantpaddune <elonpras@my.smccd.edu>
2020-04-01 13:05:18 +0530

    Connect Database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,23 @@
+require('dotenv').config()
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+mongoose
+    .connect("mongodb://localhost:27017/tshirts", { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+    .then(() => {
+        console.log("DB CONNECTS SUCCESSFULLY");
+    })
+    .catch((error) => {
+        console.error("DB GOT CRASH")
+    });
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
