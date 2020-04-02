dc85e0ce1413a35e08becf03b29d88cc5ff3d52f
prashantpaddune <elonpras@my.smccd.edu>
2020-04-03 00:27:43 +0530

    express-validator : add module, add validation

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,16 @@
 const User = require('../models/user');
+const { validationResult } = require('express-validator');
 
 exports.signUp = (req, res) => {
+
+    const errors = validationResult(req);
+    
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ 
+            errors: errors.array()[0].msg
+        });
+    }
+
     const user = new User(req.body);
     user.save((err, user) => {
         if(err){
--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 const {signUp, signOut} = require('../controllers/authentication');
 
-router.post('/signup', signUp)
+router.post('/signup', 
+    [
+        check('name').isLength({ min: 5 }).withMessage('Name must be at least 5 chars long'),
+        check('email').isEmail().withMessage('Enter correct email'),
+        check('password').isLength({ min: 5 }).withMessage('Password must be at least 5 chars long')
+    ], 
+    signUp
+);
+
 router.get('/signout', signOut);
 
 module.exports = router;
