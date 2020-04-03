17d4f55061c39fa705a8328b6e38b21363c6dd17
prashantpaddune <elonpras@my.smccd.edu>
2020-04-03 14:47:51 +0530

    Add jwt modules, create signIn route

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
 
 exports.signUp = (req, res) => {
 
@@ -25,6 +27,46 @@ exports.signUp = (req, res) => {
             email: user.email
         });
     })
+};
+
+exports.signIn = (req, res) => {
+    const { email, password } = req.body;
+
+    const errors = validationResult(req);
+    
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ 
+            errors: errors.array()[0].msg
+        });
+    };
+
+    User.findOne({email}, (err, user)=> {
+        if(err){
+            return res.status(400).json({
+                err : 'Email does not exist'
+            });
+        };
+
+        if(!user.authenticate(plainpassword)){
+            res.status(401).json({
+                err : 'Email and Password does not match'
+            });
+        };
+
+        // Create token
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+
+        // Put token in cookie
+        res.cookie('token', token, { expire: new Date() + 5555 });
+
+        // Send response to front end
+        const { _id, name, email, role } = user;
+
+        return res.json({
+            token,
+            user: { _id, name, email, role }
+        });
+    });
 };
 
 exports.signOut = (req, res) => {
--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
-const {signUp, signOut} = require('../controllers/authentication');
+const { signUp, signIn, signOut } = require('../controllers/authentication');
 
 router.post('/signup', 
     [
-        check('name').isLength({ min: 5 }).withMessage('Name must be at least 5 chars long'),
-        check('email').isEmail().withMessage('Enter correct email'),
-        check('password').isLength({ min: 5 }).withMessage('Password must be at least 5 chars long')
+        check('name')
+            .isLength({ min: 5 })
+            .withMessage('Name must be at least 5 chars long'),
+
+        check('email')
+            .isEmail()
+            .withMessage('Enter correct email'),
+
+        check('password')
+            .isLength({ min: 5 })
+            .withMessage('Password must be at least 5 chars long')
     ], 
     signUp
+);
+
+router.post('/signin', 
+    [
+        check('email')
+            .isEmail()
+            .withMessage('Enter correct email'),
+
+        check('password')
+            .isLength({ min: 5 })
+            .withMessage('Password is required')
+    ], 
+    signIn
 );
 
 router.get('/signout', signOut);
