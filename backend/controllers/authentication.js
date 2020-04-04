71a1c349bd4b4162b413005645de8e23c1d913d2
prashantpaddune <elonpras@my.smccd.edu>
2020-04-04 22:18:11 +0530

    create custom middlewares to protect routes

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -75,3 +75,30 @@ exports.signOut = (req, res) => {
         message: 'User SignOut Sucessfully'
     });
 };
+
+// Protected middleware example
+exports.isSignedIn = expressJwt({
+    secret: process.env.SECRET,
+    userProperty: "auth"
+});
+
+// Custom middlewares
+exports.isAuthenticated = (req, res, next) => {
+    let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+
+    if(!checker){
+        return res.status(403).json({
+            error: 'ACCESS DENIED'
+        });
+    };
+    next();
+};
+
+exports.isAdmin = (req, res, next) => {
+    if(req.profile === 0){
+        return res.status(403).json({
+            error: 'ACCESS DENIED, YOU ARE NOT ADMIN'
+        });
+    };
+    next();
+};
