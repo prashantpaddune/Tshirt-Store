63f7cbc595c421078a1ce495875ba8334f08b70e
prashantpaddune <elonpras@my.smccd.edu>
2020-04-05 18:55:38 +0530

    create a middleware to push order in purchase list

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -61,7 +61,35 @@ exports.userPurchasedList = (req, res) => {
                 error: 'No Order in this Account'
             });
         };
-
         return res.json(order);
     });
+};
+
+exports.pushOrderInPurchaseList = (req, res, next) => {
+    let purchases = []
+    res.body.order.products.forEach(product => {
+        purchases.push({
+            _id: product._id,
+            name: product.name,
+            description: product.description,
+            category: product.category,
+            quantity: product.quantity,
+            amount: req.body.order.amount,
+            transaction_id: req.body.order.transaction_id
+        });
+    });
+
+    User.findOneAndUpdate(
+        {_id: req.profile._id},
+        {$push: {purchases: purchases}},
+        {new: true},
+        (err, purchase) => {
+            if(err) {
+                return res.status(400).json({
+                    error: 'Unable to save purchase list'
+                });
+            };
+            next();
+        }
+    );
 };
